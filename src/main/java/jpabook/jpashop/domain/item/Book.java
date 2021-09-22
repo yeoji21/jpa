1e904e4c225fbe0ce0d23e09dc23409abe04c2cc
여지원 <yooara0508@naver.com>
2021-09-22 15:54:14 +0900

    jpa 응용 1편 코드 완성

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,18 +1,30 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Address;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
 @DiscriminatorValue("B")
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Book extends Item{
 
     private String author;
     private String isbn;
 
+    public static Book bookOf(Item item, String author, String isbn){
+        Book book = new Book();
+        book.setName(item.getName());
+        book.setPrice(item.getPrice());
+        book.setStockQuantity(item.getStockQuantity());
+        book.author = author;
+        book.isbn = isbn;
+        return book;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -43,4 +43,5 @@ public abstract class Item {
         }
         this.stockQuantity = restStock;
     }
+
 }
